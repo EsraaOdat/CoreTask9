4c262b9a5acd939ac242e05523ffe6e766fe9e73
user name <esraaodat123123@gmail.com>
2024-09-02 22:30:37 -0700

    Task9

---
+++ backend/Task3/DTOs/UsersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3.DTOs
+{
+    public class UsersDTO
+    {
+        public string Username { get; set; } = null!;
+
+        public string? Email { get; set; } = null!;
+        public string? Password { get; set; }
+    }
+}
